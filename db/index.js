ef338aa07569e3904cf8552779c110680c0c24f5
dafonegai <dafonegai@gmail.com>
2026-01-11 11:24:40 +0545

    added db credentials with dotenv

--- db/index.js
+++ db/index.js
@@ -1,11 +1,9 @@
-const {Pool}= require("pg");
+require("dotenv").config();
 
-const pool= new Pool({
-    host: "localhost",
-    port: 5432,
-    user:"backend_user",
-    password:"backend_pass",
-    database:"backend_db",
-})
-
-module.exports = pool;
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
